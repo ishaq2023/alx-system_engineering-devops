9e4875f362297a0625e9904f645f72f44cff6e00
ishaq2023 <ishaqlagnaoui@gmail.com>
2024-01-30 08:27:53 -0800

    0x15. API

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
         with open("todo_all_employees.json", "w") as jsonfile:
             json.dump({
-                 u.get("id"): [{
+                u.get("id"): [{
                       "task": t.get("title"),
                       "completed": t.get("completed"),
                       "username": u.get("username")
